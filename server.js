2267d5dadd71264957996c6c853a8454a8bdbd70
Fatimah Haruna <Fatimaharuna2007@gmail.com>
2026-04-07 14:07:27 +0100

    Updated

--- server.js
+++ server.js
@@ -6,3 +6,6 @@ app.get('/', (req, res) => {
   res.json({'message': 'Welcome to my Blog API'});
 }); 
 
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+}); 
