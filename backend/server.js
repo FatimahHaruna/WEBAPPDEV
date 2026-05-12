8dbe3621aa72c97e7610b148faa476588cde7238
FatimahHaruna <Fatimaharuna2007@gmail.com>
2026-05-12 21:41:10 +0100

    Changes made and installed react

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-// const connectDB = require('./config/db');
-// const todoRoutes = require('./routes/todos');
-// const errorHandler = require('./middleware/errorHandler');
+const connectDB = require('./config/db');
+const todoRoutes = require('./routes/todos');
+const errorHandler = require('./middleware/errorHandler');
 
 dotenv.config();
 const app = express();
 
 // Connect to Database
-// connectDB();
+connectDB();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Routes
-// app.use('/api/todos', todoRoutes);
+app.use('/api/todos', todoRoutes);
 
 // Error Handler Middleware (must be last)
-// app.use(errorHandler);
+app.use(errorHandler);
 
 // Test route to verify server is running
 app.get('/', (req, res) => {
