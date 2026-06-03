4ee749f3eede0b2933c63cd653e4235a972d6405
FatimahHaruna <Fatimaharuna2007@gmail.com>
2026-06-03 11:33:20 +0100

    Refactor asynchronous JavaScript examples for clarity and functionality

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -1,23 +1,23 @@
 //Asynchronous JavaScript
-// setTimeout(() => {
-//     console.log("This message is displayed after 3 seconds.");
-// }, 3000);   
+setTimeout(() => {
+    console.log("This message is displayed after 3 seconds.");
+}, 3000);   
 
-// console.log("Start")
-// setTimeout(() => {
-//     console.log("This message is displayed after 2 seconds.");
-// }, 3000)
-// console.log('End');
+console.log("Start")
+setTimeout(() => {
+    console.log("This message is displayed after 2 seconds.");
+}, 3000)
+console.log('End');
 
 // //Callbacks
-// function fetchData(callback) {
-//     setTimeout(() => {
-//         callback("Data loaded");
-//     }, 2000);
-// }
-// fetchData((data) => {
-//     console.log(data);
-// });
+function fetchData(callback) {
+    setTimeout(() => {
+        callback("Data loaded");
+    }, 2000);
+}
+fetchData((data) => {
+    console.log(data);
+});
 
 function greet(name) {
     console.log(`Hello,${name}!`)
@@ -28,3 +28,17 @@ function greetUser(callback) {
     callback(name)
 }
 greetUser(greet)
+
+//Promises
+const promise = new Promise((resolve, reject) => {
+    const success = true;
+    if(success){
+        resolve("Successful");
+    }
+    else{        
+        reject("Failed");
+    }
+}) 
+
+.then(result => console.log(result))
+.catch(error => console.error(error));
